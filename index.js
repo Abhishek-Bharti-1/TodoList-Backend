cbc15eb729d9349fd7a0e31b3009abad3b3346ec
Abhishek Bharti <96315588+Abhishek-Bharti-1@users.noreply.github.com>
2025-01-03 12:51:37 +0530

    f

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("", (req, res)=>{
 app.get('/tasks', async (req, res) => {
   try {
     const tasks = await Task.find();
-    res.json(tasks);
+    return res.json(tasks);
   } catch (err) {
     res.status(500).json({ error: 'Error fetching tasks' });
   }
